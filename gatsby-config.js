fbd3cf6ccf860485c64f1adc37d7287fd2f49b73
Alexandra Taylor <alexandrataylor@MacBook-Pro-8.local>
2019-09-14 12:31:27 +0200

    Initial commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,18 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Design+Code 3',
+    description: 'Complete courses about the best tools and design systems. Prototype and build apps with React and swift. 60 hours of video content and resource materials. No coding experience required.',
+    keywords: 'react course, react for designers, ios development, sketsch app, swift app course, arkit2, after effects, create sketch plug in'
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet', 
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'jukr7hhqwo7a',
+        accessToken: 'sV_PF63URCqBOfoRxRkmwu8O9HKuerqzkjD21TPxGBU'
+      }
+    }
+  ],
 }
+
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -4,28 +4,23 @@ import Helmet from 'react-helmet'
 
 import Header from '../components/header'
 import './index.css'
+import Footer from '../components/Footer'
 
 const Layout = ({ children, data }) => (
   <div>
     <Helmet
       title={data.site.siteMetadata.title}
       meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
+        { name: 'description', content: data.site.siteMetadata.description },
+        { name: 'keywords', content: data.site.siteMetadata.keywords },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <Header />
       {children()}
+      <Footer data ={data}>
+Backgrounds made in Cinema 4D, iOS app inSwift, site in React. <a href="mailto:support@designcode.io "> Email us</a> to ask anything. © 2019
+      </Footer>
     </div>
-  </div>
 )
 
 Layout.propTypes = {
@@ -39,6 +34,17 @@ export const query = graphql`
     site {
       siteMetadata {
         title
+        description
+        keywords
+      }
+    }
+    allContentfulLink(sort:{ fields: [createdAt], order:ASC}) {
+      edges {
+        node {
+          title
+          url
+          createdAt
+        }
       }
     }
   }
